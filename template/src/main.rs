a54ffa4e351284ae031c1a2d52e681b386cb136e
Fowdre <foudre1000@gmail.com>
2025-12-01 03:20:30 -0500

    feat: updates template

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,18 +1,19 @@
-// Uses template from commit <>
-
 mod file_reading;
 use file_reading::load_file;
+
+mod part1;
+mod part2;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     match load_file(&args) {
         Ok(input) => {
             if args[1] == "1" {
-                let part1_result = part1_solution(&input);
+                let part1_result = part1::solution(&input);
 
                 println!("Part 1 solution: {}", part1_result);
             } else if args[1] == "2" {
-                let part2_result = part2_solution(&input);
+                let part2_result = part2::solution(&input);
 
                 println!("Part 2 solution: {}", part2_result);
             } else {
@@ -25,12 +26,4 @@ fn main() {
             std::process::exit(1);
         }
     }
-}
-
-fn part1_solution(input: &[String]) -> u128 {
-    todo!("Implement solution for part 1")
-}
-
-fn part2_solution(input: &[String]) -> u128 {
-    todo!("Implement solution for part 2")
 }
---
+++ template/src/part2.rs
@@ -0,0 +1,3 @@
+pub fn solution(input: &[String]) -> u128 {
+    todo!("Implement solution for part 2")
+}
